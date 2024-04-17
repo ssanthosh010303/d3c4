8faf8a8c846b557f0ee57524bcf6603b39432ae1
Sakthi Santhosh <116057110+sakthisanthosh010303@users.noreply.github.com>
2024-04-17 07:31:30 +0100

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Author: Sakthi (Sandy) Santhosh
+ * Created on: 11/04/2024
+ *
+ * Day-3 Challenge-4: Length of User's Name
+ */
+namespace Challenge4;
+
+class Program
+{
+    static void Main()
+    {
+        string? userName;
+
+        Console.Write("Enter name: ");
+        userName = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(userName))
+        {
+            int nameLength = userName.Trim().Length;
+
+            Console.WriteLine("\nResult");
+            Console.WriteLine($"  Length: " + nameLength);
+        }
+    }
+}
